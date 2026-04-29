3b5ace23e67943cee192812457cfd361111a44b9
Narlyx <shout@narlyx.dev>
2026-04-29 00:31:20 -0600

    Gaming is for losers

--- hosts/acetylene/default.nix
+++ hosts/acetylene/default.nix
@@ -2,6 +2,14 @@
   imports = [
     ./hardware-configuration.nix
     ./graphics.nix
-    modules.roles.game
+    modules.roles.desktop
+    modules.features.virtualisation
+    modules.features.narlyx.creative
+  ];
+
+  services.flatpak.enable = true;
+  programs.steam.enable = true;
+  home-manager.users.narlyx.home.packages = with pkgs; [
+    prismlauncher
   ];
 }
